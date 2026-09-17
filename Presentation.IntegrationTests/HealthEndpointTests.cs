86aca3c6971c720bbffbfdc471cd8d320f694407
David <td.ta.david@gmail.com>
2026-09-17 15:14:49 +0200

    feat: HealthEndpointTests

---
+++ Presentation.IntegrationTests/HealthEndpointTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net;
+
+namespace Presentation.IntegrationTests
+{
+    public sealed class HealthEndpointTests(WebApplicationFactory<Program> factory) : IClassFixture<WebApplicationFactory<Program>>
+    {
+        [Fact]
+        public async Task GetHealthReturnsOk()
+        {
+            var client = factory.CreateClient();
+
+            var response = await client.GetAsync(new Uri("/health", UriKind.Relative));
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+    }
+}
